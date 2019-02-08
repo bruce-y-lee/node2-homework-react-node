65ed5f13a84d8d9cdb1b3faa75950b65b0a6372d
yg lee <yglee@ygs-MacBook-Air.local>
2019-02-08 15:28:02 -0800

    Edited index.js

--- index.js
+++ index.js
@@ -39,7 +39,16 @@ app.use(passport.session());
 require('./routes/authroutes')(app);
 require('./routes/billingRoutes')(app);
 
-
+if(process.env.NODE_ENV === 'production') {
+    //express will serve up production assets like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+    //Express will serve up the index.html file
+    //if it deosn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      });
+}
 
 
 
