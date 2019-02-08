da9e0fc95a5af201c06c149a9d0019af0aff1967
yg lee <yglee@ygs-MacBook-Air.local>
2019-02-08 15:22:35 -0800

    Edited prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,15 +1,14 @@
 //production keys here
 module.exports = {
     googleClientID : process.env.GOOGLE_CLIENT_ID,
-    //"670980002889-drrjr91j31v05gli12ri1m41uafpj00h.apps.googleusercontent.com",
+   
     googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
-    //"OSkeA_Wlf-OyQLt4MgNT762i",
+   
     mongoURI: process.env.MONGO_URI,
-    //"mongodb://alexander:123asd@ds211275.mlab.com:11275/node2-emaily-dev",
+   
     cookieKey: process.env.COOKIE_KEY,
-    //"asdfsdfhkjasdhf83asndvmasndfoi332kdnjsn"
+   
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
 
-//mongodb://adminUser:123asd@ds157762.mlab.com:57762/node2-emaily-prod
